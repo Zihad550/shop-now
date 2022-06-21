b8de90e8e19967bdc5c62c250f2f94e8237b312a
Zihad550 <hussainmuzahid83@gmail.com>
2022-06-21 06:49:24 +0600

    feat: product page completed

--- src/views/Products/Products.vue
+++ src/views/Products/Products.vue
@@ -1,6 +1,26 @@
 <template>
-  <div v-if="products">
-    {{ products }}
+  <div
+    v-if="products"
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 container mx-auto my-5"
+  >
+    <!-- {{ products }} -->
+    <!-- product -->
+    <div
+      class="shadow-md hover:shadow-lg hover:cursor-pointer"
+      v-for="product in products"
+      style="max-width: 300px"
+      :key="product._id"
+    >
+      <img :src="product.src" alt="" />
+      <!-- body -->
+      <div class="flex justify-between p-3">
+        <div>
+          <h4>{{ product.name }}</h4>
+          <p class="mt-1 text-sm text-gray-500">{{ product.colour }}</p>
+        </div>
+        <p class="text-sm font-medium text-gray-900">$ {{ product.price }}</p>
+      </div>
+    </div>
   </div>
   <div v-else>...loading</div>
 </template>
