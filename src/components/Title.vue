2e6c08a3b2e2e169e3b32b1233b8232ae7d1dbbe
Zihad550 <hussainmuzahid83@gmail.com>
2022-06-21 09:33:26 +0600

    feat: hero section created

---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1
+    class="title text-4xl text-center my-5 border-b-black border-b-2 w-max mx-auto"
+  >
+    <slot />
+  </h1>
+</template>
+
+<style scoped>
+.title {
+  font-family: "Kdam Thmor Pro", sans-serif;
+}
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <h1>Top Categories</h1>
+  <Hero />
+  <Title>Top Categories</Title>
   <Categories />
 </template>
 
 <script>
+import Title from "@/components/Title.vue";
 import Categories from "./Categories/Categories.vue";
+import Hero from "./Hero/Hero.vue";
 
 export default {
   components: {
     Categories,
+    Title,
+    Hero,
   },
 };
 </script>
